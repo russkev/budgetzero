ae97a32aab8f483664bfadf4e61b5f5233d5d216
Kevin Russell <kev_kix@hotmail.com>
2022-09-06 09:09:04 +1000

    Added checkbox to updated table

--- src/components/TransactionView/ToggleChecked.vue
+++ src/components/TransactionView/ToggleChecked.vue
@@ -2,22 +2,23 @@
   <v-simple-checkbox 
     :value="isSelected"
     :ripple="false"
-    :disabled="expanded.length > 0"
+    :disabled="expandedTransactions.length > 0"
     v-on="$listeners"
   />
 </template>
 
 <script>
+import { mapGetters } from "vuex"
+
 export default {
   props: {
     isSelected: {
       type: Boolean,
       default: false,
     },
-    expanded: {
-      type: Array,
-      default: []
-    }
   },
+  computed: {
+    ...mapGetters("accountTransactions", ["expandedTransactions"])
+  }
 };
 </script>
--- src/components/TransactionView/TransactionsTable.vue
+++ src/components/TransactionView/TransactionsTable.vue
@@ -5,6 +5,7 @@
     v-model="selected"
     :headers="dataTableHeaders"
     :items="transactions"
+    group-by="date"
     item-key="_id"
     show-select
     single-expand
@@ -13,77 +14,109 @@
     :options.sync="options"
     :server-items-length="numTransactionsTotal"
     :items-per-page="itemsPerPage"
+    :footer-props="{
+      'items-per-page-options': [5, 10, 20, 50, 100, 200],
+      'update.options': options,
+    }"
   >
+    <template #group.header="{items}">
+      <td colspan="20" class="date-row">
+        {{ items[0].date }}
+      </td>
+    </template>
+    <template #item="{ item, expand, select, isSelected }">
+      <tr id="transaction-edit-row" v-if="item._id === editedTransaction._id" :key="item._id">
+        <td id="edit-row-checkbox" class="expanded-checkbox">
+          <toggle-checked :is-selected="isSelected" @input="select($event)" />
+        </td>
+      </tr>
+      <tr class="transaction-row" data-testid="transaction-row" v-else>
+        <td id="edit-row-checkbox" class="expanded-checkbox">
+          <toggle-checked :is-selected="isSelected" @input="select($event)" />
+        </td>
+      </tr>
+    </template>
   </v-data-table>
 </template>
 
 <script>
 import { mapGetters, mapMutations, mapActions } from "vuex";
-import { DEFAULT_TRANSACTIONS_PER_PAGE } from '../../constants';
+import { DEFAULT_TRANSACTIONS_PER_PAGE } from "../../constants";
+import ToggleChecked from "./ToggleChecked.vue";
 export default {
-  computed: {
-    ...mapGetters("accountTransactions", [
-      "accountId",
-      "accountOptions",
-      "selectedTransactions",
-      "expandedTransactions",
-      "dataTableHeaders",
-      "transactions",
-      "numServerTransactions",
-      "itemsPerPage",
-      "isCreatingNewTransaction",
-    ]),
-    ...mapGetters(["selectedBudgetId", "accounts"]),
-    numTransactionsTotal() {
-      return this.isCreatingNewTransaction
-        ? this.numServerTransactions + 1
-        : this.numServerTransactions
+    computed: {
+        ...mapGetters("accountTransactions", [
+            "accountId",
+            "accountOptions",
+            "editedTransaction",
+            "selectedTransactions",
+            "expandedTransactions",
+            "dataTableHeaders",
+            "transactions",
+            "numServerTransactions",
+            "itemsPerPage",
+            "isCreatingNewTransaction",
+        ]),
+        ...mapGetters(["selectedBudgetId", "accounts"]),
+        numTransactionsTotal() {
+            return this.isCreatingNewTransaction
+                ? this.numServerTransactions + 1
+                : this.numServerTransactions;
+        },
+        selected: {
+            get() {
+                return this.selectedTransactions;
+            },
+            set(transactions) {
+                this.SET_SELECTED_TRANSACTIONS(transactions);
+            },
+        },
+        expanded: {
+            get() {
+                return this.expandedTransactions;
+            },
+            set(transactions) {
+                this.SET_EXPANDED_TRANSACTIONS(transactions);
+            },
+        },
+        options: {
+            get() {
+                return this.accountOptions;
+            },
+            set(updated_options) {
+                this.SET_ACCOUNT_OPTIONS(updated_options);
+            },
+        },
     },
-    selected: {
-      get() {
-        return this.selectedTransactions;
-      },
-      set(transactions) {
-        this.SET_SELECTED_TRANSACTIONS(transactions);
-      },
+    watch: {
+        options: {
+            handler(current, prev) {
+                if (current.itemsPerPage !== prev.itemsPerPage) {
+                    localStorage.setItem("transactionsPerPage", current.itemsPerPage);
+                }
+            },
+            deep: true,
+        },
     },
-    expanded: {
-      get() {
-        return this.expandedTransactions;
-      },
-      set(transactions) {
-        this.SET_EXPANDED_TRANSACTIONS(transactions);
-      },
+    created() {
+        const saved_transactions_per_page = localStorage.getItem("transactionsPerPage");
+        if (saved_transactions_per_page) {
+            this.SET_ITEMS_PER_PAGE(parseInt(saved_transactions_per_page));
+        }
+        else {
+            this.SET_ITEMS_PER_PAGE(DEFAULT_TRANSACTIONS_PER_PAGE);
+        }
     },
-    options: {
-      get() {
-        return this.accountOptions;
-      },
-      set(updated_options) {
-        this.SET_ACCOUNT_OPTIONS(updated_options);
-      },
+    methods: {
+        ...mapMutations("accountTransactions", [
+            "SET_ACCOUNT_ID",
+            "SET_SELECTED_TRANSACTIONS",
+            "SET_EXPANDED_TRANSACTIONS",
+            "SET_ACCOUNT_OPTIONS",
+            "SET_ITEMS_PER_PAGE",
+        ]),
+        ...mapActions("accountTransactions", ["getTransactions"]),
     },
-  },
-  created() {
-    const saved_transactions_per_page = localStorage.getItem("transactionsPerPage")
-    if (saved_transactions_per_page) {
-      this.SET_ITEMS_PER_PAGE(parseInt(saved_transactions_per_page))
-    } else {
-      this.SET_ITEMS_PER_PAGE(DEFAULT_TRANSACTIONS_PER_PAGE)
-    }
-  },
-  methods: {
-    ...mapMutations("accountTransactions", [
-      "SET_ACCOUNT_ID",
-      "SET_SELECTED_TRANSACTIONS",
-      "SET_EXPANDED_TRANSACTIONS",
-      "SET_ACCOUNT_OPTIONS",
-      "SET_ITEMS_PER_PAGE"
-    ]),
-    ...mapActions("accountTransactions", [
-      "getTransactions",
-    ])
-
-  },
+    components: { ToggleChecked }
 };
 </script>
