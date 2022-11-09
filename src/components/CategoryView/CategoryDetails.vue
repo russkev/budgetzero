e21cf8a294ee510c26cadc290fddb92775651a15
Kevin Russell <kev_kix@hotmail.com>
2022-11-10 07:46:01 +1100

    Category details transactions memos have ellipsis

--- src/components/CategoryView/CategoryDetails.vue
+++ src/components/CategoryView/CategoryDetails.vue
@@ -1,48 +1,56 @@
 <template>
-  <v-card
-    width="100%"
-    flat
-    color="background lighten-1"
-    class="category-details-title ma-0 ml-2"
-  >
+  <v-card width="100%" flat color="background lighten-1" class="category-details-title ma-0 ml-2">
     <v-card-title class="primary darken-3 pa-3">Working</v-card-title>
     <categories-working />
     <v-card-title class="primary darken-3 pa-3">Transactions</v-card-title>
-    <v-data-table
-      :headers="transactionHeaders"
-      :items="monthTransactions"
-    />
+    <v-data-table :headers="transactionHeaders" :items="monthTransactions">
+      <template #item="{ item }">
+        <tr>
+          <td>
+            {{ item.date }}
+          </td>
+          <td>
+            <div class="ellipsis">{{ item.memo }}</div>
+          </td>
+          <td>
+            {{ item.account }}
+          </td>
+          <td>
+            {{ item.amount }}
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
   </v-card>
 </template>
 
 <script>
-import CategoriesWorking from "./CategoriesWorking.vue";
-import { mapGetters, mapActions } from "vuex";
-
+import CategoriesWorking from './CategoriesWorking.vue'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  name: "CategoryDetails",
+  name: 'CategoryDetails',
   components: {
-    CategoriesWorking,
+    CategoriesWorking
   },
   mounted() {
-    this.getMonthTransactions();
+    this.getMonthTransactions()
   },
   computed: {
-    ...mapGetters(["masterCategoriesById", "selectedBudgetId"]),
-    ...mapGetters("categoryMonth", ["selectedMonth", "transactionHeaders", "monthTransactions"]),
+    ...mapGetters(['masterCategoriesById', 'selectedBudgetId']),
+    ...mapGetters('categoryMonth', ['selectedMonth', 'transactionHeaders', 'monthTransactions']),
     transactions() {
-      let mc = this.masterCategoriesById;
+      let mc = this.masterCategoriesById
       this.getMonthTransactions().then(() => {
-        return this.monthTransactions;
+        return this.monthTransactions
       })
     }
   },
   methods: {
-    ...mapActions(["fetchTransactionsForMonth"]),
-    ...mapActions("categoryMonth", ["getMonthTransactions"]),
+    ...mapActions(['fetchTransactionsForMonth']),
+    ...mapActions('categoryMonth', ['getMonthTransactions'])
   }
-};
+}
 </script>
 
 <style>
